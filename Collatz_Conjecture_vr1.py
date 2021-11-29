bfbe4292abfd7cdfebf6c2f415513c63bb6213d1
Kayla Casper-Dixon <94490295+KMPotvin@users.noreply.github.com>
2021-11-29 09:31:44 -0500

    Add files via upload
    Completed Version 1. Tkinter built GUI. Shows the max peak of the Collatz Conjecture and the number of iterations required to reach the 4-2-1 loop.

---
+++ Collatz_Conjecture_vr1.py
@@ -0,0 +1,98 @@
+import tkinter as tk
+import tkinter.font as tkfont
+from PIL import ImageTk, Image
+import array as arr
+
+
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        tk.Frame.__init__(self, master, bg='black')
+        self.grid()
+        self.createwidgets()
+
+    def scrollhandler(self, *L):
+        op, howMany = L[0], L[1]
+        if op == 'scroll':
+            units = L[2]
+            self.entry.xview_scroll(howMany, units)
+        elif op == 'moveto':
+            self.entry.xview_moveto(howMany)
+
+    def cleartext(self):
+        self.entry.configure(state='normal')
+        self.entry.delete(0, 'end')
+        return
+
+    def click(self):
+        self.entry.configure(state='disabled')
+        workingNum = self.entry.get()
+        num_list = collatz_conjecture(workingNum)
+        iterations = len(num_list)
+        maxi = max(num_list)
+        self.return_entry.config(text=f'{iterations} cycles is what it took to reach the 4-2-1 loop. Highest peak was {maxi}.')
+
+    def createwidgets(self):
+        centurygoth36 = tkfont.Font(family="Century Gothic", size=12, weight="bold")
+        top = self.winfo_toplevel()
+        top.rowconfigure(0, weight=1)
+        top.columnconfigure(0, weight=1)
+        self.rowconfigure(0, weight=1)
+        self.columnconfigure(0, weight=1)
+
+        self.img = Image.open(r"C:\Users\witha\OneDrive\Desktop\Github_Projects\Python_Programming\Collatz Conjecture\labelheader.png")
+        reduce_image = self.img.resize((600, 200), Image.ANTIALIAS)
+        self.smaller_img = ImageTk.PhotoImage(reduce_image)
+        imageLabel= tk.Label(self, image=self.smaller_img, anchor='center')
+        imageLabel.grid(row=0, column=0, columnspan=4)
+
+        self.entry = tk.Entry(self, width=30)
+        self.entry.grid(row=6)
+        self.entryScroll = tk.Scrollbar(orient=tk.HORIZONTAL, command=self.scrollhandler)
+        self.entry['xscrollcommand'] = self.entryScroll.set
+
+        numlabel = tk.Label(self, text='What number would you like to test?', fg='green', bg='black')
+        numlabel.grid(row=6, column=0, stick=tk.W)
+
+        self.showButton = tk.Button(self, text='Calculate', command=self.click, bg='green', fg='white')
+        self.showButton.grid(row=6, sticky=tk.E)
+        self.showButton['font'] = centurygoth36
+
+        self.return_entry = tk.Label(self, fg='green', bg='black', width=60)
+        self.return_entry.grid(row=8, columnspan=4)
+        self.return_entry['font'] = centurygoth36
+
+        self.tryagainButton = tk.Button(self, text='Try Again!', width=20, command=self.cleartext, bg='green', fg='white')
+        self.tryagainButton.grid(row=17, column=0, sticky=tk.W)
+        self.tryagainButton['font'] = centurygoth36
+
+        self.quitButton = tk.Button(self, text='Quit', width=20, command=self.quit, bg='green', fg='white')
+        self.quitButton.grid(row=17)
+        self.quitButton['font'] = centurygoth36
+
+
+def collatz_conjecture(base_num):
+    number_array = arr.array('L')
+    hailstone = int(base_num)
+    number_array.append(hailstone)
+    while hailstone != 1:
+        if hailstone % 2 != 0:
+            hailstone = (3 * hailstone) + 1
+            number_array.append(hailstone)
+        else:
+            hailstone = hailstone // 2
+            number_array.append(hailstone)
+
+    return number_array
+
+
+def main():
+    app = Application()
+    app.master.title('Collatz Conjecture Tool')
+    app['bg'] = 'black'
+    app.mainloop()
+
+
+if __name__ == '__main__':
+    main()
+
+
